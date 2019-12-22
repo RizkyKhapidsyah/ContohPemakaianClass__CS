3f826c9abb44f5f789da13f134c44d8cb6eb8b8d
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2019-12-23 05:03:01 +0700

    Add project files.

---
+++ Mobil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContohPemakaianClass__CS
+{
+    class Mobil
+    {
+        public string Warna;
+        public int Kecepatan;
+
+        public void Klakson()
+        {
+            Console.WriteLine("Telolett");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ContohPemakaianClass__CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil Bus = new Mobil();
+            Mobil Angkot = new Mobil();
+
+            Bus.Warna = "Merah";
+            Bus.Kecepatan = 800;
+            Bus.Klakson();
+
+            Angkot.Warna = "Kuning";
+            Angkot.Kecepatan = 300;
+            Angkot.Klakson();
+
+            Console.WriteLine("Warna Mobil Bus Adalah     : " + Bus.Warna);
+            Console.WriteLine("Warna Mobil Angkot Adalah  : " + Angkot.Warna);
+
+            Console.ReadLine();
+        }
+    }
+}
